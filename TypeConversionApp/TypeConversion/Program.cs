81181b7173eb463b5b7f2b3fea6b06ff0c143347
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-01-09 21:58:09 +0000

    Basics of type conversion

---
+++ TypeConversionApp/TypeConversion/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TypeConversion;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What is your age?: ");
+        string? ageText = Console.ReadLine();
+        //Console.WriteLine(ageText + 15);
+        //int age = int.Parse(ageText);
+
+        bool isValidInt = int.TryParse(ageText, out int age);
+        Console.WriteLine($"The input value: {ageText} and can be converted: {isValidInt}");
+        Console.WriteLine(age + 15);
+        double testDouble = age;
+        decimal testDecimal = (decimal)testDouble; //explicit casting
+        Console.ReadLine();
+    }
+}
+
