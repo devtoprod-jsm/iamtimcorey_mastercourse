5398804b141bb8f8d92afa74ffee642540226ac7
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-01-15 06:04:42 +0000

    Completed basics of do while loops

---
+++ DoWhileLoopApp/DoWhileLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DoWhileLoop;
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool isValidAge;
+        int age;
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("What is your age? ");
+            string? ageText = Console.ReadLine();
+            isValidAge = int.TryParse(ageText, out age);
+            if (isValidAge == false)
+            {
+                Console.WriteLine("Press Enter and input a valid age.");
+                Console.ReadLine();
+            }
+        } while (isValidAge == false);
+        Console.ReadLine();
+    }
+}
+
---
+++ DoWhileLoopTestApp/DoWhileLoopTest/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace DoWhileLoopTest;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string? firstName = "";
+        string? wantToExit = "";
+        do
+        {
+            Console.Clear();
+            Console.Write("Please enter your name: ");
+            firstName = Console.ReadLine();
+            if (firstName.ToLower() == "jai")
+            {
+                Console.WriteLine($"Hello professor {firstName}");
+            }
+            else
+            {
+                Console.WriteLine($"Hello {firstName}");
+            }
+            Console.Write("If you want to exit type in yes or y or exit: ");
+            wantToExit = Console.ReadLine();
+            Console.WriteLine(wantToExit);
+            Console.ReadLine();
+        } while (wantToExit.ToLower() != "yes" && wantToExit.ToLower() != "exit" && wantToExit.ToLower() != "y");
+        Console.ReadLine();
+    }
+}
+
