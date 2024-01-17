3955ef73e4b697c5209ab93b0c6771c2b23d96e1
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-01-17 22:54:52 +0000

    Completed basics of lists

---
+++ ListsApp/Lists/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Lists;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //string[] firstNames = new string[5];
+        List<string> firstNames = new List<string>();
+        firstNames.Add("Jai");
+        firstNames.Add("Sind");
+        firstNames.Add("Mo");
+        Console.WriteLine(firstNames[firstNames.Count - 1]);
+        string data = "tella,nangineni";
+        List<string> lastNames = data.Split(",").ToList();
+        lastNames.Add("jagarlamudi");
+        Console.WriteLine(lastNames[0]);
+        Console.ReadLine();
+    }
+}
+
---
+++ ListsTestApp/ListsTest/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+//define variables
+List<string> firstNames = new List<string>();
+bool toExit = true;
+
+do
+{
+    Console.Clear();
+    Console.Write("Do you want start entering names? If so enter y or press any key: ");
+    string decision = "";
+    decision = Console.ReadLine();
+    if (decision.ToLower() == "y")
+    {
+        toExit = false;
+    }
+    else
+    {
+        toExit = true;
+    }
+if (toExit == false)
+    {
+        Console.Write("Please enter the student firstName: ");
+        string? name = "";
+        Console.ReadLine();
+        firstNames.Add(name);
+    }
+} while (toExit == false);
+Console.WriteLine("Thanks for using the app.");
+Console.WriteLine($"The total number of students entered is: {firstNames.Count}");
+Console.ReadLine();
