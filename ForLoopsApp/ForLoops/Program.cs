c2db73fceddc369fadb6732211205520b090dcd7
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-01-18 07:09:44 +0000

    Completed basics of for loop

---
+++ ForLoopsApp/ForLoops/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+string data = "jai,sind,mo";
+List<string> firstNames = new();
+firstNames = data.Split(",").ToList();
+for (int i=0; i < firstNames.Count; i++)
+{
+    Console.WriteLine($"Hello {firstNames[i]}");
+}
+decimal total=0;
+List<decimal> deposits = new();
+deposits.Add(12.88M);
+deposits.Add(88.12M);
+deposits.Add(123);
+for (int i=0; i < deposits.Count; i++)
+{
+    total += deposits[i];
+}
+Console.WriteLine($"The total deposit is {total}");
+Console.ReadLine();
+
