04b1a52f269cde1b4ddec57b9bc76b083c02e76b
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2024-01-19 11:37:04 +0000

    Completed basics of foreach loop

---
+++ ForEachLoopApp/ForEachLoop/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+List<string> firstNames = new List<string>();
+bool noMoreEntries = false;
+do
+{
+    Console.Write("If you want to enter firstName please press 'y' or press any other key: ");
+    string? decision = Console.ReadLine();
+    if (decision.ToLower() == "y")
+    {
+        Console.Write("Please enter the firstname: ");
+        firstNames.Add(Console.ReadLine());
+
+    }
+
+    else
+    {
+        noMoreEntries = true;
+    }
+   
+} while (noMoreEntries == false);
+if (firstNames.Count == 0)
+{
+    Console.WriteLine("Sorry no entries found.");
+}
+else
+{
+    foreach (string firsName in firstNames)
+    {
+        Console.WriteLine($"Hello {firsName}");
+    }
+}
+Console.ReadLine();
